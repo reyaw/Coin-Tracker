d59015c323e8ce5a68934e78a37a15620c12e4da
reyaw <reyaw@protonmail.com>
2021-04-28 18:34:17 -0400

    development build 1

---
+++ bitcoin_notifications.py
@@ -0,0 +1,62 @@
+import requests
+import time
+from datetime import datetime
+
+IFTTT_WEBHOOKS_URL = 'https://maker.ifttt.com/trigger/{}/with/key/di9LGZrw5TAcILa5deoyzE'
+BITCOIN_API_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+BITCOIN_PRICE_THRESHOLD = 50000
+
+
+def get_latest_bitcoin_price():
+    response = requests.get(BITCOIN_API_URL)
+    response_json = response.json()
+    return response_json['bpi']['USD']['rate_float']
+
+
+def post_ifttt_webhook(event, value):
+    # The payload that will be sent to IFTTT service
+    data = {'value1': value}
+    # Inserts our desired event
+    ifttt_event_url = IFTTT_WEBHOOKS_URL.format(event)
+    # Sends a HTTP POST request to the webhook URL
+    requests.post(ifttt_event_url, json=data)
+
+
+def main():
+    bitcoin_history = []
+    while True:
+        price = get_latest_bitcoin_price()
+        date = datetime.now()
+        bitcoin_history.append({'date': date, 'price': price})
+
+        # Send an emergency notification
+        if price < BITCOIN_PRICE_THRESHOLD:
+            post_ifttt_webhook('bitcoin_price_emergency', price)
+
+        # Send a Telegram notification
+        # Once we have 5 items in our bitcoin_history send an update
+        if len(bitcoin_history) == 5:
+            post_ifttt_webhook('bitcoin_price_update', format_bitcoin_history(bitcoin_history))
+            # Reset the history
+            bitcoin_history = []
+
+        # Sleep for 5 minutes
+        time.sleep(5*60*60)
+
+
+def format_bitcoin_history(bitcoin_history):
+    rows = []
+    for bitcoin_price in bitcoin_history:
+        # Formats the data into a string: '24.02.2021 15.09'
+        date = bitcoin_price['date'].strftime('%d.%m.%Y %H:%M')
+        price = bitcoin_price['price']
+
+        row = '{}: $<b>{}</b>'.format(date, price)
+        rows.append(row)
+
+    return '<br>'.join(rows)
+
+
+if __name__ == '__main__':
+    main()
+get_latest_bitcoin_price()
--- webhook.py
+++ webhook.py
@@ -1,4 +1,4 @@
 import requests
 
-ifttt_webhook_url = 'https://maker.ifttt.com/trigger/test_event/with/key/di9LGZrw5TAcILa5deoyzE'
-requests.post(ifttt_webhook_url)
+ifttt_webhook_url = 'https://maker.ifttt.com/trigger/bepis/with/key/di9LGZrw5TAcILa5deoyzE'
+print(requests.post(ifttt_webhook_url))
